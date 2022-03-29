340358f57fa087f195cc048dddc19837a13412b4
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-29 11:03:39 +0400

    hsdfbgjshd whefad

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "azad";
+            int count = 21;
+            Console.WriteLine(ExtentionMethods.IsOdd(count));
+            Console.WriteLine(ExtentionMethods.IsEven(count));
+            Console.WriteLine(ExtentionMethods.IsContainsDigit(name));
+            Console.WriteLine(ExtentionMethods.ToCapitalize(name));
+            Console.WriteLine(ExtentionMethods.GetValueIndexes(name, 'a'));
+
+
+            Console.ReadLine();
+        }
+    }
+}
